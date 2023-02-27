bddd58516f21654d91bdd5ebe97d80c2618a3c1b
cinta <cintatafur@gmail.com>
2023-02-27 17:26:47 +0100

    ⚡️ Add(service):en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { PushService } from './services/push.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private push: PushService,  private platform: Platform,) {  this.iniApp(); }
+
+  iniApp() {
+    this.platform.ready().then(() => {
+      
+      this.push.confInit();
+    });
+  }
 }
