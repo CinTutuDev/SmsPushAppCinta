9468e862c30c32486b34f8032c6620d3d99d2ce6
cinta <cintatafur@gmail.com>
2023-02-28 10:36:18 +0100

    ✅ creo metodo(   notificacionRecibida)

--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 /* import OneSignal from 'onesignal-cordova-plugin'; */
 import { environment } from 'src/environments/environment';
-import OneSignal from 'onesignal-cordova-plugin';
+import { OneSignal, OSNotification } from '@awesome-cordova-plugins/onesignal/ngx'
+/* import OSNotification from 'onesignal-cordova-plugin/dist/OSNotification'; */
+/* import OneSignal from 'onesignal-cordova-plugin'; */
 @Injectable({
   providedIn: 'root',
 })
@@ -17,11 +19,29 @@ export class PushService {
   OneSignalAppId: string = environment.oneSignal;
   idRemitenteFire: string = environment.idFireremi;
 
-  constructor() {}
+  constructor(private oneSignal: OneSignal) {}
 
-  oneSignalInit(): void{
+  async oneSignalInit(){
+    this.oneSignal.startInit(this.OneSignalAppId, this.idRemitenteFire);
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
+    
+    this.oneSignal.handleNotificationReceived().subscribe(async(noti) => {
+      console.log('Notificació n recibida', noti );
+
+      this.notificacionRecibida(noti);
+     // do something when notification is received
+    });
+    
+    this.oneSignal.handleNotificationOpened().subscribe((noti) => {
+      console.log('Notificación abierta', noti );
+      // do something when a notification is opened
+    });
+    
+    this.oneSignal.endInit();
+
     //OneSignal.setLogLevel(6, 0);
-    OneSignal.setAppId(this.OneSignalAppId);
+    /* OneSignal.setAppId('97d69f25-f6ac-440c-ad0a-352465c01b7c');
     OneSignal.setNotificationOpenedHandler( jsonData => {
         console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
     });
@@ -32,8 +52,18 @@ export class PushService {
  
     OneSignal.getDeviceState( resp => {
       //this.storage.set('push_id', resp.userId);
-    });
+    }); */
   }
  
+   notificacionRecibida( noti : OSNotification) {
+
+   const payload = noti.payload;
+   const siExiste = this.sms.find(msg => msg.notificationID === payload?.notificationID)
+
+   if(siExiste){ return; }
+
+   this.sms.unshift(payload);
+
+  }
 
 }
